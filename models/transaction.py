9ece3d45ccd22c87ac2233b4cb7482e2a67ce96f
bobokun <jon.cy.lee98@gmail.com>
2024-07-21 21:25:27 -0400

    Add some validations for missing values

--- models/transaction.py
+++ models/transaction.py
@@ -1,10 +1,20 @@
+from datetime import datetime
+from typing import Optional
+
 from pydantic import BaseModel
+from pydantic import validator
 
 
 class Transaction(BaseModel):
-    account: str
-    amount: float
-    date: str
-    payee: str
-    notes: str = None
-    cleared: bool = False
+    account: str = ""
+    amount: Optional[float] = 0
+    date: Optional[str] = datetime.now().strftime("%b %d, %Y")
+    payee: Optional[str] = ""
+    notes: Optional[str] = ""
+    cleared: Optional[bool] = False
+
+    @validator("amount", pre=True)
+    def empty_str_to_zero(cls, v):
+        if v == "":
+            return 0
+        return v
