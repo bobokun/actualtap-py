a5fcb921ad47b516b36cca41ecfe36142cb0fcd0
bobokun <jon.cy.lee98@gmail.com>
2024-07-29 18:59:38 -0400

    optimize sending multiple transactions to actual

--- api/transactions.py
+++ api/transactions.py
@@ -14,14 +14,15 @@ router = APIRouter()
 
 
 @router.post("/", dependencies=[Depends(get_api_key)])
-def add_transaction(transactions: Union[Transaction, List[Transaction]]):
+def add_transactions(transactions: Union[Transaction, List[Transaction]]):
     try:
         if isinstance(transactions, Transaction):
             transactions = [transactions]
 
         for transaction in transactions:
-            transaction.amount *= Decimal(-1)
-            actual_service.add_transaction(**transaction.dict())
+            transaction.amount *= Decimal(-1)  # Invert the amount
+
+        actual_service.add_transactions(transactions)
 
         return {"message": "Transactions added successfully"}
     except ValueError as ve:
--- services/actual_service.py
+++ services/actual_service.py
@@ -1,5 +1,6 @@
 import json
 from decimal import Decimal
+from typing import List
 
 from actual import Actual
 from actual.queries import create_transaction
@@ -9,6 +10,7 @@ from core.config import settings
 from core.logs import MyLogger
 from core.security import generate_custom_id
 from core.util import convert_to_date
+from models.transaction import Transaction
 
 logger = MyLogger()
 
@@ -17,42 +19,49 @@ class ActualService:
     def __init__(self):
         self.client = None
 
-    def add_transaction(self, account: str, amount: float, date: str, payee: str, notes: str = None, cleared: bool = False):
+    def add_transactions(self, transactions: List[Transaction]):
+        transaction_info_list = []
+        submitted_transactions = []
         with Actual(settings.actual_url, password=settings.actual_password, file=settings.actual_budget) as actual:
-            actual_acount_id = settings.account_mappings.get(account, settings.actual_default_account_id)
-            date = convert_to_date(date)
-            import_id = generate_custom_id()
-            transaction_info = {
-                "Account": account,
-                "Account_ID": actual_acount_id,
-                "Amount": str(Decimal(amount)),
-                "Date": str(date),
-                "Imported ID": import_id,
-                "Payee": payee,
-                "Notes": notes,
-                "Cleared": cleared,
-            }
-            logger.info("\n" + json.dumps(transaction_info, indent=4))
-            # validate account_id
-            if not actual_acount_id:
-                raise ValueError(f"Account name '{account}' is not mapped to an Actual Account ID.")
-            if not payee:
-                payee = settings.actual_backup_payee
-            t = create_transaction(
-                s=actual.session,
-                account=actual_acount_id,
-                amount=Decimal(amount),
-                date=date,
-                imported_id=import_id,
-                payee=payee,
-                notes=notes,
-                cleared=cleared,
-                imported_payee=payee,
-            )
+            for t in transactions:
+                actual_acount_id = settings.account_mappings.get(t.account, settings.actual_default_account_id)
+                date = convert_to_date(t.date)
+                import_id = generate_custom_id()
+                transaction_info = {
+                    "Account": t.account,
+                    "Account_ID": actual_acount_id,
+                    "Amount": str(Decimal(t.amount)),
+                    "Date": str(date),
+                    "Imported ID": import_id,
+                    "Payee": t.payee,
+                    "Notes": t.notes,
+                    "Cleared": t.cleared,
+                }
+                transaction_info_list.append(transaction_info)
+                # validate account_id
+                if not actual_acount_id:
+                    raise ValueError(f"Account name '{t.account}' is not mapped to an Actual Account ID.")
+                if not t.payee:
+                    payee = settings.actual_backup_payee
+                else:
+                    payee = t.payee
+                t = create_transaction(
+                    s=actual.session,
+                    account=actual_acount_id,
+                    amount=Decimal(t.amount),
+                    date=date,
+                    imported_id=import_id,
+                    payee=t.payee,
+                    notes=t.notes,
+                    cleared=t.cleared,
+                    imported_payee=payee,
+                )
+                submitted_transactions.append(t)
             rs = get_ruleset(actual.session)
-            rs.run(t)
+            rs.run(submitted_transactions)
+            logger.info("\n" + json.dumps(transaction_info_list, indent=4))
             actual.commit()
-            return t
+            return transaction_info_list
 
 
 # Initialize the service
